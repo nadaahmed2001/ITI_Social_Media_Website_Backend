024c67cb1a4c0646ae44826336b03f86c04203a5
Tarek Hassanin <hassanin.trk@gmail.com>
2025-04-28 22:25:36 +0300

    fix: added profile serach url

--- ITIHub/users/urls.py
+++ ITIHub/users/urls.py
@@ -61,6 +61,8 @@ urlpatterns = [
 
     path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    path('search/', ProfileSearchView.as_view(), name='profile_search'),  # For searching profiles by username
 
 ]
 
