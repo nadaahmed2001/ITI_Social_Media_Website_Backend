49832ba2e96d6dfdd0ed6fdfb300f03d62986235
Nizar-Esmat <nizaresmat2000@gmail.com>
2025-04-29 13:58:39 +0300

    edit in core view

--- core/views.py
+++ core/views.py
@@ -2,7 +2,16 @@ from django.http import JsonResponse
 import os
 
 def websocket_config(request):
+    ws_host = os.environ.get('WS_HOST', request.get_host())
+    enable_websocket = os.environ.get('ENABLE_WEBSOCKET', 'false').lower() == 'true'
+    
+    # Add more debugging information
     return JsonResponse({
-        'wsHost': os.environ.get('WS_HOST', request.get_host()),
-        'enableWebsocket': os.environ.get('ENABLE_WEBSOCKET', 'false').lower() == 'true'
+        'wsHost': ws_host,
+        'enableWebsocket': enable_websocket,
+        'request_host': request.get_host(),
+        'env_variables_set': {
+            'WS_HOST': 'WS_HOST' in os.environ,
+            'ENABLE_WEBSOCKET': 'ENABLE_WEBSOCKET' in os.environ
+        }
     })
