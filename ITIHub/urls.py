aa931ef0746ef2c7d29f04879edcb7b07944f6bd
Nada Ahmed <nada052001@gmail.com>
2025-05-02 15:43:24 +0300

    merge

--- ITIHub/urls.py
+++ ITIHub/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse, JsonResponse
+
 import logging
 
 # Configure logging
