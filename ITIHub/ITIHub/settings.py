4ca1135b470ad96f7831edcedbae9a3155cd9def
Nada Ahmed <nada052001@gmail.com>
2025-05-04 22:59:04 +0300

    Fix errors in railway.json

--- ITIHub/ITIHub/settings.py
+++ ITIHub/ITIHub/settings.py
@@ -26,6 +26,8 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 # Use environment variables for allowed hosts
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+if "healthcheck.railway.app" not in ALLOWED_HOSTS:
+    ALLOWED_HOSTS.append("healthcheck.railway.app")
 # Remove healthcheck.railway.app
 if 'healthcheck.railway.app' in ALLOWED_HOSTS:
     ALLOWED_HOSTS.remove('healthcheck.railway.app')
