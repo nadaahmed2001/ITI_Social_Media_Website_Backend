ea3b0a204ebd66398a99171a8d3febdaf80ce761
Nada Ahmed <nada052001@gmail.com>
2025-05-05 13:40:08 +0300

    Fix CSRF

--- ITIHub/users/views.py
+++ ITIHub/users/views.py
@@ -94,7 +94,7 @@ class RegisterStudentView(APIView):
 class LoginView(APIView):
     permission_classes = [AllowAny]  # Allow unauthenticated users to access this view
 
-    @csrf_exempt
+    # @csrf_exempt
     def post(self, request):
         print("Inside POST in login view")
         serializer = LoginSerializer(data=request.data)
