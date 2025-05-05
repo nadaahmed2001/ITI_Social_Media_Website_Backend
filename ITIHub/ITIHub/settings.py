293e5a327654d595e6b504afd2eaecf7a749a520
Nada Ahmed <nada052001@gmail.com>
2025-05-05 13:50:27 +0300

    Fix CSRF

--- ITIHub/ITIHub/settings.py
+++ ITIHub/ITIHub/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://itisocialmediawebsitebackend-production.up.railway.app"
+    "https://itisocialmediawebsitebackend-production.up.railway.app",
+    "https://iti-social-media-websi-git-2fec63-nada-ahmeds-projects-9fb624b0.vercel.app"
 ]
 CSRF_COOKIE_SECURE = True  # Ensures CSRF cookie only sent over HTTPS
 SESSION_COOKIE_SECURE = True  # Ensures session cookie only sent over HTTPS
