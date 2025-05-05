36f51a618b9d07ba6f0a191e204f8b0859d1632d
Nada Ahmed <nada052001@gmail.com>
2025-05-05 12:55:14 +0300

    Fix

--- ITIHub/ITIHub/settings.py
+++ ITIHub/ITIHub/settings.py
@@ -34,6 +34,8 @@ print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 CSRF_TRUSTED_ORIGINS = [
     "https://itisocialmediawebsitebackend-production.up.railway.app"
 ]
+CSRF_COOKIE_SECURE = True  # Ensures CSRF cookie only sent over HTTPS
+SESSION_COOKIE_SECURE = True  # Ensures session cookie only sent over HTTPS
 
 # User model configuration
 AUTH_USER_MODEL = "users.User"
